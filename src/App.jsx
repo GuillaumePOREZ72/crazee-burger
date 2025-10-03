9890a9a7e3b9306f0f0991d4e2f840ef167bc8c5
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-10-03 09:59:04 +0200

    ✨ F01: LOGIN > Create LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-
+import LoginPage from "./components/pages/LoginPage";
 
 function App() {
   // state (état, données)
 
+  // comportements
+
+  //affichage (render)
+  return (
+    <>
+      <LoginPage />
+    </>
+  );
 }
 
 export default App;
